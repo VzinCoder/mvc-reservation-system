f35d1c18973486927dfd88bf0f110443f706abb0
VzinCoder <contato.viniciusp.b@gmail.com>
2024-12-08 12:59:41 -0300

    add page create

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,11 +1,18 @@
 const router = require('express').Router()
 const EmployeeController = require('../controller/EmployeeController')
+const { createEmployeeValidator } = require('../validator/employeeValidator')
 
 router.get('/', (req, res, next) => {
     EmployeeController.getEmployee(req, res, next)
 })
 
+router.get('/create', (req, res, next) => {
+    EmployeeController.getPageCreateEmployee(req, res, next)
+})
 
+router.post('/create', createEmployeeValidator(), (req, res, next) => {
+    EmployeeController.postCreateEmployee(req, res, next)
+})
 
 
 module.exports = router
