034cb19dc32e0eb5bb7c4f04be2a3815ec1d7ecb
VzinCoder <contato.viniciusp.b@gmail.com>
2024-12-08 10:25:12 -0300

    add new migration to fix table employee

---
+++ migrations/1733664147525_add-column-salary-to-employee.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.addColumn('employee',{
+        salary:{type:'decimal(10,2)',notNull:true}
+    })
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropColumn('employee','salary')
+};
