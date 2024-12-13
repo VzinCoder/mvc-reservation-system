b7cdb5dd92d176eb80aeb0641d575c0365202ef8
VzinCoder <contato.viniciusp.b@gmail.com>
2024-12-13 17:37:38 -0300

    added customer model

---
+++ src/model/CustomerModel.js
@@ -0,0 +1,21 @@
+
+
+class CustomerModel {
+
+    static async findAll() {
+        const { rows } = await pool.query(`SELECT * FROM customer`)
+        return rows
+    }
+
+    static async create({ id, name, phone, cpf, email }) {
+        const query = `INSERT INTO employee (id,name,phone,cpf,email) values ($1,$2,$3,$4,$5)`
+        const result = await pool.query(query, [id, name, phone, cpf, email])
+        if (result.rowCount === 0) {
+            throw new Error(`Failed to create customer : ${result.command}`)
+        }
+        return true
+    }
+
+}
+
+module.exports = CustomerModel
